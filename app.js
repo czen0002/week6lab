fade623097aa0f24bcb636a47f5ff0e345839d41
chengzeng <czen0002@student.monash.edu>
2019-09-04 10:48:27 +1000

    aa

--- app.js
+++ app.js
@@ -133,8 +133,8 @@ app.post('/update', function(req, res){
 app.get('/deleteOldComplete', function(req, res){
     let now = Date.now();
     console.log(now);
-    
-    col.deleteMany({taskStatus: "Complete", taskDueDate: {$lt: now}}, function(err, result){
+    // taskStatus: "Complete", 
+    col.deleteMany({taskDueDate: {$lt: now}}, function(err, result){
         if (err) {
             res.redirect('/404');
         } else {
