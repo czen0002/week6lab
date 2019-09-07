3be7716805c8867d6db673fa6f31d101f6f2c5ff
czen0002 <czen0002@student.monash.edu>
2019-09-07 23:10:43 +1000

    update task.js

---
+++ models/task.js
@@ -0,0 +1,31 @@
+let mongoose = require('mongoose');
+
+let taskSchema = mongoose.Schema({
+    taskName: {
+        type: String,
+        require: true
+    },
+    taskAssign: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'DeveloperCol'
+    },
+    taskDueDate: {
+        type: Date,
+        require: true
+    },
+    taskStatus: {
+        type: String,
+        require: true
+    },
+    taskDescription: {
+        type: String
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+let taskModel = mongoose.model("TaskCol", taskSchema);
+
+module.exports = taskModel;
